b5a0db26ef197ae9e965c4608584c80051af4edb
Glen Kelley <glen.t.kelley@gmail.com>
2016-01-31 09:32:49 +1100

    Added deploy-frontend environment parsing.

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -35,14 +35,40 @@ if [[ -n "$VERBOSE" ]] ; then
 fi
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
-DEST=~/Sites/battlecode
+DEST_DIR=~/Sites/battlecode
 if [[ -n "$WEB_BUILD_DEST" ]]; then
-  DEST="$WEB_BUILD_DEST"
+  DEST_DIR="$WEB_BUILD_DEST"
 fi
 
-echo "Copying web files to $DEST"
-mkdir -p ${DEST}
-rm -rf ${DEST}/*
-cp -r "${GIT_ROOT}/internal/web/." "$DEST/." 
+echo "Copying web files to ${DEST_DIR}"
+mkdir -p ${DEST_DIR}
+rm -rf ${DEST_DIR}/*
+
+#Simple no substitution
+#cp -r "${GIT_ROOT}/internal/web/." "$DEST/." 
+
+SOURCE_DIR="${GIT_ROOT}/internal/web"
+REPLACE_EXT="html js"
+find "$SOURCE_DIR" | while read FILE ; do
+	DEST_FILE="${FILE/$SOURCE_DIR/$DEST_DIR}"
+	if [[ -d "$FILE" ]] ; then
+		mkdir -p "$DEST_FILE"
+	elif [[ -f "$FILE" ]] ; then
+		COPIED=
+		for EXT in $REPLACE_EXT ; do
+			if [[ "$FILE" == *\.$EXT ]] ; then
+				sed "s/localhost:8080/api.akusete.com:8080/g" "$FILE" > "$DEST_FILE"
+				#chmod --reference "$FILE" "$DEST_FILE"
+				COPIED="TRUE"
+				break
+			fi
+		done
+		if [[ -z "$COPIED" ]] ; then
+			cp "$FILE" "$DEST_FILE"
+		fi
+	else
+		exit 1
+	fi
+done
 
 echo "Success."
